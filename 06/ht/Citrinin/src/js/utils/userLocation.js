5d56351412315dce0fe585af3d879060bca12e26
Kate Kuzkina <Kate.Kuzkina@gmail.com>
2018-07-09 11:49:23 +0300

    change provider of user location

--- 06/ht/Citrinin/src/js/utils/userLocation.js
+++ 06/ht/Citrinin/src/js/utils/userLocation.js
@@ -1,6 +1,9 @@
 import request from './request';
 
-var userLocation = request('http://ip-api.com/json').then(data => ({ latitude: data.lat, longitude: data.lon }))
+var userLocation = request('https://ipinfo.io/geo/?token=2ef535515b7afb').then(data => {
+    let coords = data.loc.split(',');
+    return { latitude: coords[0], longitude: coords[1] };
+})
 
 
 export default userLocation;
